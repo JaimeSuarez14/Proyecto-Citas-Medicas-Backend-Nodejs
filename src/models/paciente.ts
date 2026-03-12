772698a6e52f0b9bee33ae12a843022e7b3c14ae
Jaime Suarez <jaisua8@gmail.com>
2026-03-11 21:46:28 -0500

    Creacion del modelo de intefaces pacientes

--- src/models/paciente.ts
+++ src/models/paciente.ts
@@ -7,6 +7,45 @@ export interface IPaciente {
   apellidos: string;
   edad: number;
   cedula: string;
-  telefono: string
+  telefono: string;
   direccion: string;
 }
+
+interface PacienteCreationAttributes extends Optional<IPaciente, "id"> {}
+
+interface PacienteInstance
+  extends Model<IPaciente, PacienteCreationAttributes>, IPaciente {}
+
+export const Pacientes = sequelize.define<PacienteInstance>("Pacientes", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nombres: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellidos: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  edad: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+
+  cedula: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  telefono: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  direccion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
